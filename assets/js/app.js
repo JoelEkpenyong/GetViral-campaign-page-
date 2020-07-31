8b31e54f5c90d29afd4d9129306f4860da2e0734
JoelEkpenyong <joelekpenyong@gmail.com>
2020-07-31 11:43:40 +0100

    changed to video and image upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,23 @@ Dropzone.options.myAwesomeDropzone = {
       });
     }
   };
+
+  Dropzone.options.myDrop1 = {
+    init: function() {
+      this.on("addedfile", function(file) { 
+        file.previewElement.addEventListener('click', function(){
+            document.querySelector('.dz-preview').remove()
+        })
+      });
+    }
+  };
+
+  Dropzone.options.myDrop = {
+    init: function() {
+      this.on("addedfile", function(file) { 
+        file.previewElement.addEventListener('click', function(){
+            document.querySelector('.dz-preview').remove()
+        })
+      });
+    }
+  };
