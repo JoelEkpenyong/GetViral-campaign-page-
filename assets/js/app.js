46052608388937116fec04b40b30eb25d50481fc
JoelEkpenyong <joelekpenyong@gmail.com>
2020-07-31 20:05:24 +0100

    updated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,4 @@
-fileUpload = document.querySelector('#my-awesome-dropzone')
 
-Dropzone.options.myAwesomeDropzone = {
-    init: function() {
-      this.on("addedfile", function(file) { 
-        file.previewElement.addEventListener('click', function(){
-            document.querySelector('.dz-preview').remove()
-        })
-      });
-    }
-  };
 
   Dropzone.options.myDrop1 = {
     init: function() {
